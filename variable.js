92526009c7a88ef080a1614a3382fe63264a0dab
abusayadebu <abusayad743@gmail.com>
2026-01-22 22:53:25 +0600

    js arithmetic operations practice

---
+++ variable.js
@@ -0,0 +1,22 @@
+// var name = "ebu";
+// var isStudent = true;
+// var roll = 12;
+// var goal = "to be a goo web developer";
+
+// console.log(name, isStudent, goal);
+// ----------------------
+
+// problem-1 -- You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.---
+
+var givenMoney = 2000;
+
+var orangePrice = 300;
+var applePrice = 400;
+
+var totalprice = orangePrice + applePrice;
+
+var remainingMoney = givenMoney - totalprice; 
+console.log(remainingMoney);
+
+// - Problem-2 --
+
